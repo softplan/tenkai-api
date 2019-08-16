3d06edbd20d72a03ccc02b7b5387f86b03f2fe3f
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-08-16 17:42:57 -0300

    Canary screen

---
+++ dbms/model/traffic.go
@@ -0,0 +1,20 @@
+package model
+
+//TrafficRequest structure
+type TrafficRequest struct {
+	EnvironmentID      int                     `json:"environmentId"`
+	Domain             string                  `json:"domain"`
+	ServiceName string `json:"serviceName"`
+	ContextPath        string                  `json:"contextPath"`
+	HeaderName         string                  `json:"headerName"`
+	HeaderValue        string                  `json:"headerValue"`
+	HeaderReleaseName  string                  `json:"headerReleaseName"`
+	DefaultReleaseName string                  `json:"defaultReleaseName"`
+	Releases           []TrafficReleaseRequest `json:"releases"`
+}
+
+//TrafficReleaseRequest Structure
+type TrafficReleaseRequest struct {
+	Name   string `json:"name"`
+	Weight int `json:"weight"`
+}
