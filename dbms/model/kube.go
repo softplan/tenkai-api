08e2ba15b36eb4f81eccbc59665d07abe40b9a14
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-08-26 23:18:59 -0300

    Added services feature

--- dbms/model/kube.go
+++ dbms/model/kube.go
@@ -9,7 +9,22 @@ type Pod struct {
 	Age      string `json:"age"`
 }
 
+//Service structure
+type Service struct {
+	Name       string `json:"name"`
+	Type       string `json:"type"`
+	ClusterIP  string `json:"clusterIP"`
+	ExternalIP string `json:"externalIP"`
+	Ports      string `json:"ports"`
+	Age        string `json:"age"`
+}
+
 //PodResult structure
 type PodResult struct {
 	Pods []Pod `json:"pods"`
+}
+
+//ServiceResult structure
+type ServiceResult struct {
+	Services []Service `json:"services"`
 }
