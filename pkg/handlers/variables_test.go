9de0ddbe7c714a8f7f802049cd56b5ad9e169b0e
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-10-21 15:39:48 -0300

    add edit variable test

---
+++ pkg/handlers/variables_test.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/softplan/tenkai-api/pkg/constraints"
+	"github.com/softplan/tenkai-api/pkg/dbms/model"
+	"github.com/softplan/tenkai-api/pkg/dbms/repository/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestEditVariable(t *testing.T) {
+
+	appContext := AppContext{}
+	appContext.K8sConfigPath = "/tmp/"
+
+	var payload model.DataVariableElement
+	payload.Data.Secret = false
+	payload.Data.Name = "my_variable"
+	payload.Data.Description = "my_description"
+	payload.Data.Scope = "my_chart"
+	payload.Data.Value = "my_value"
+	payload.Data.EnvironmentID = 1
+
+	mockVariableDAO := &mocks.VariableDAOInterface{}
+	mockVariableDAO.On("EditVariable", mock.Anything).Return(nil)
+
+	appContext.Repositories = Repositories{}
+	appContext.Repositories.VariableDAO = mockVariableDAO
+
+	payloadStr, _ := json.Marshal(payload)
+
+	req, err := http.NewRequest("POST", "/variables", bytes.NewBuffer(payloadStr))
+	assert.NoError(t, err)
+	assert.NotNil(t, req)
+
+	roles := []string{constraints.TenkaiVariablesSave}
+	principal := model.Principal{Name: "alfa", Email: "beta@gmail.com", Roles: roles}
+	pSe, _ := json.Marshal(principal)
+	req.Header.Set("principal", string(pSe))
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(appContext.editVariable)
+	handler.ServeHTTP(rr, req)
+
+	mockVariableDAO.AssertNumberOfCalls(t, "EditVariable", 1)
+	assert.Equal(t, http.StatusCreated, rr.Code, "Response is not Ok.")
+
+}
