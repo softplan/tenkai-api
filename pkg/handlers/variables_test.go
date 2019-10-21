e3147ebd65213515af25fef29dd68ced1b05e6dd
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-10-21 17:22:46 -0300

    remove code smells

--- pkg/handlers/variables_test.go
+++ pkg/handlers/variables_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"github.com/softplan/tenkai-api/pkg/constraints"
 	"github.com/softplan/tenkai-api/pkg/dbms/model"
 	"github.com/softplan/tenkai-api/pkg/dbms/repository/mocks"
@@ -49,5 +50,65 @@ func TestEditVariable(t *testing.T) {
 
 	mockVariableDAO.AssertNumberOfCalls(t, "EditVariable", 1)
 	assert.Equal(t, http.StatusCreated, rr.Code, "Response is not Ok.")
+
+}
+
+func TestDeleteVariable(t *testing.T) {
+
+	appContext := AppContext{}
+	appContext.K8sConfigPath = "/tmp/"
+
+	mockVariableDAO := &mocks.VariableDAOInterface{}
+	mockVariableDAO.On("DeleteVariable", mock.Anything).Return(nil)
+
+	appContext.Repositories = Repositories{}
+	appContext.Repositories.VariableDAO = mockVariableDAO
+
+	req, err := http.NewRequest("DELETE", "/variables/delete/1", nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, req)
+
+	roles := []string{constraints.TenkaiVariablesDelete}
+	principal := model.Principal{Name: "alfa", Email: "beta@gmail.com", Roles: roles}
+	pSe, _ := json.Marshal(principal)
+	req.Header.Set("principal", string(pSe))
+
+	rr := httptest.NewRecorder()
+	r := mux.NewRouter()
+	r.HandleFunc("/variables/delete/{id}", appContext.deleteVariable).Methods("DELETE")
+	r.ServeHTTP(rr, req)
+
+	mockVariableDAO.AssertNumberOfCalls(t, "DeleteVariable", 1)
+	assert.Equal(t, http.StatusOK, rr.Code, "Response is not Ok.")
+
+}
+
+func TestGetVariables(t *testing.T) {
+
+	appContext := AppContext{}
+	appContext.K8sConfigPath = "/tmp/"
+
+	mockVariableDAO := &mocks.VariableDAOInterface{}
+	mockVariableDAO.On("DeleteVariable", mock.Anything).Return(nil)
+
+	appContext.Repositories = Repositories{}
+	appContext.Repositories.VariableDAO = mockVariableDAO
+
+	req, err := http.NewRequest("GET", "/variables/delete/1", nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, req)
+
+	roles := []string{constraints.TenkaiVariablesDelete}
+	principal := model.Principal{Name: "alfa", Email: "beta@gmail.com", Roles: roles}
+	pSe, _ := json.Marshal(principal)
+	req.Header.Set("principal", string(pSe))
+
+	rr := httptest.NewRecorder()
+	r := mux.NewRouter()
+	r.HandleFunc("/variables/delete/{id}", appContext.deleteVariable).Methods("DELETE")
+	r.ServeHTTP(rr, req)
+
+	mockVariableDAO.AssertNumberOfCalls(t, "DeleteVariable", 1)
+	assert.Equal(t, http.StatusOK, rr.Code, "Response is not Ok.")
 
 }
