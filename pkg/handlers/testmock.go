ab387bcb9f7429389a9e4adb3491182a08df23bd
Anderson Custódio <afcustodioo@gmail.com>
2019-11-01 15:27:44 -0300

    add test scenarios for variablevalues.go (#17)
    * Partial commit
    
    * Added TestGetVariablesByEnvironmentAndScope
    
    * Add functions to be reused in tests.
    
    * Add test for getVariablesByEnvironmentAndScope handler.
    
    * Fix variables and functions visibility.

--- pkg/handlers/testmock.go
+++ pkg/handlers/testmock.go
@@ -1,42 +1,46 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"net/http"
+	"net/http/httptest"
+	"testing"
 
 	mockAud "github.com/softplan/tenkai-api/pkg/audit/mocks"
 	"github.com/softplan/tenkai-api/pkg/dbms/model"
 	mockRepo "github.com/softplan/tenkai-api/pkg/dbms/repository/mocks"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-//MockPrincipal injects a http header with the specified role to be used only for testing.
-func MockPrincipal(req *http.Request, roles []string) {
+//mockPrincipal injects a http header with the specified role to be used only for testing.
+func mockPrincipal(req *http.Request, roles []string) {
 	principal := model.Principal{Name: "alfa", Email: "beta@alfa.com", Roles: roles}
 	pSe, _ := json.Marshal(principal)
 	req.Header.Set("principal", string(pSe))
 }
 
-//MockGetByID mocks a call to GetByID function to be used only for testing.
-func MockGetByID(appContext *AppContext) *mockRepo.EnvironmentDAOInterface {
+//mockGetByID mocks a call to GetByID function to be used only for testing.
+func mockGetByID(appContext *AppContext) *mockRepo.EnvironmentDAOInterface {
 	mockEnvDao := &mockRepo.EnvironmentDAOInterface{}
-	env := MockGetEnv()
+	env := mockGetEnv()
 	mockEnvDao.On("GetByID", int(env.ID)).Return(&env, nil)
 	appContext.Repositories.EnvironmentDAO = mockEnvDao
 	return mockEnvDao
 }
 
-//MockGetByIDError mocks a call to GetByID function returning an error to be used only for testing.
-func MockGetByIDError(appContext *AppContext) *mockRepo.EnvironmentDAOInterface {
+//mockGetByIDError mocks a call to GetByID function returning an error to be used only for testing.
+func mockGetByIDError(appContext *AppContext) *mockRepo.EnvironmentDAOInterface {
 	mockEnvDao := &mockRepo.EnvironmentDAOInterface{}
 	mockEnvDao.On("GetByID", mock.Anything).Return(nil, errors.New("Some error"))
 	appContext.Repositories.EnvironmentDAO = mockEnvDao
 	return mockEnvDao
 }
 
-//MockGetEnv returns an Environment struct to be used only for testing.
-func MockGetEnv() model.Environment {
+//mockGetEnv returns an Environment struct to be used only for testing.
+func mockGetEnv() model.Environment {
 	var env model.Environment
 	env.ID = 999
 	env.Group = "foo"
@@ -49,8 +53,8 @@ func MockGetEnv() model.Environment {
 	return env
 }
 
-//MockVariable returns a Variable struct to be used only for testing.
-func MockVariable() model.Variable {
+//mockVariable returns a Variable struct to be used only for testing.
+func mockVariable() model.Variable {
 	var variable model.Variable
 	variable.Scope = "global"
 	variable.Name = "username"
@@ -61,11 +65,59 @@ func MockVariable() model.Variable {
 	return variable
 }
 
-//MockDoAudit mocks a call to DoAudit function to be used only for testing.
-func MockDoAudit(appContext *AppContext, operation string, auditValues map[string]string) *mockAud.AuditingInterface {
+//mockDoAudit mocks a call to DoAudit function to be used only for testing.
+func mockDoAudit(appContext *AppContext, operation string, auditValues map[string]string) *mockAud.AuditingInterface {
 	mockAudit := &mockAud.AuditingInterface{}
 	mockAudit.On("DoAudit", mock.Anything, mock.Anything, "beta@alfa.com", operation, auditValues)
 	appContext.Auditing = mockAudit
 
 	return mockAudit
+}
+
+func mockGetAllVariablesByEnvironmentAndScope(appContext *AppContext) *mockRepo.VariableDAOInterface {
+	mockVariableDAO := &mockRepo.VariableDAOInterface{}
+	var variables []model.Variable
+	variable := mockVariable()
+	variables = append(variables, variable)
+	mockVariableDAO.On("GetAllVariablesByEnvironmentAndScope", int(variable.EnvironmentID), mock.Anything).Return(variables, nil)
+
+	appContext.Repositories.VariableDAO = mockVariableDAO
+
+	return mockVariableDAO
+}
+
+//testHandlerFunc should be used only for testing.
+type testHandlerFunc func(http.ResponseWriter, *http.Request)
+
+func commonTestUnmarshalPayloadError(t *testing.T, endpoint string, handFunc testHandlerFunc) *httptest.ResponseRecorder {
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer([]byte(`["invalid": 123]`)))
+	assert.NoError(t, err)
+
+	mockPrincipal(req, []string{"tenkai-variables-save"})
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handFunc)
+	handler.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func commonTestHasAccessError(t *testing.T, endpoint string, handFunc testHandlerFunc, appContext *AppContext) *httptest.ResponseRecorder {
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer([]byte(`{"data":[{"environmentId":999}]}`)))
+	assert.NoError(t, err)
+
+	mockPrincipal(req, []string{"tenkai-variables-save"})
+
+	var envs []model.Environment
+	envs = append(envs, mockGetEnv())
+	mockEnvDao := mockGetByID(appContext)
+	mockEnvDao.On("GetAllEnvironments", "beta@alfa.com").Return(nil, errors.New("Record not found"))
+
+	appContext.Repositories.EnvironmentDAO = mockEnvDao
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handFunc)
+	handler.ServeHTTP(rr, req)
+
+	return rr
 }
