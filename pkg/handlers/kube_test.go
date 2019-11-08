5a09fa786116775253a71555e39a1157b729d8f5
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-11-08 13:32:18 -0300

    add kube tests

--- pkg/handlers/kube_test.go
+++ pkg/handlers/kube_test.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/softplan/tenkai-api/pkg/constraints"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -27,6 +28,60 @@ func TestServices(t *testing.T) {
 	rr := httptest.NewRecorder()
 	r := mux.NewRouter()
 	r.HandleFunc("/listServices/{id}", appContext.services).Methods("GET")
+	r.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code, "Response is not Ok.")
+	mockConvention.AssertNumberOfCalls(t, "GetKubeConfigFileName", 1)
+
+}
+
+func TestPods(t *testing.T) {
+
+	appContext := AppContext{}
+
+	mockEnvDao := mockEnvDaoWithLotOfThings(&appContext)
+	mockConvention := mockConventionInterface(&appContext)
+	mockHelmSvc := mockHelmSvcWithLotOfThings(&appContext)
+
+	appContext.Repositories = Repositories{}
+	appContext.Repositories.EnvironmentDAO = mockEnvDao
+	appContext.HelmServiceAPI = mockHelmSvc
+
+	req, err := http.NewRequest("GET", "/listPods/12", nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, req)
+
+	rr := httptest.NewRecorder()
+	r := mux.NewRouter()
+	r.HandleFunc("/listPods/{id}", appContext.pods).Methods("GET")
+	r.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code, "Response is not Ok.")
+	mockConvention.AssertNumberOfCalls(t, "GetKubeConfigFileName", 1)
+
+}
+
+func TestDeletePods(t *testing.T) {
+
+	appContext := AppContext{}
+
+	mockEnvDao := mockEnvDaoWithLotOfThings(&appContext)
+	mockConvention := mockConventionInterface(&appContext)
+	mockHelmSvc := mockHelmSvcWithLotOfThings(&appContext)
+
+	appContext.Repositories = Repositories{}
+	appContext.Repositories.EnvironmentDAO = mockEnvDao
+	appContext.HelmServiceAPI = mockHelmSvc
+
+	req, err := http.NewRequest("DELETE", "/deletePod?environmentID=999&podName=alfa", nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, req)
+
+	mockPrincipal(req, []string{constraints.TenkaiAdmin})
+
+	rr := httptest.NewRecorder()
+	r := mux.NewRouter()
+	r.HandleFunc("/deletePod", appContext.deletePod).Methods("DELETE")
 	r.ServeHTTP(rr, req)
 
 	assert.Equal(t, http.StatusOK, rr.Code, "Response is not Ok.")
