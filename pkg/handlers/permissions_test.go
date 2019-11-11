f02e5c821210d2a11b10fdc920142067e6952475
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-11-11 11:34:19 -0300

    add permissions unit tests

---
+++ pkg/handlers/permissions_test.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/softplan/tenkai-api/pkg/constraints"
+	"github.com/softplan/tenkai-api/pkg/dbms/repository/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewEnvironmentPermission(t *testing.T) {
+
+	appContext := AppContext{}
+
+	mockUserDAO := mocks.UserDAOInterface{}
+	mockUserDAO.On("AssociateEnvironmentUser", mock.Anything, mock.Anything).Return(nil)
+
+	appContext.Repositories.UserDAO = &mockUserDAO
+
+	req, err := http.NewRequest("GET", "/permissions/users/10/environments/99", nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, req)
+
+	mockPrincipal(req, []string{constraints.TenkaiAdmin})
+
+	rr := httptest.NewRecorder()
+	r := mux.NewRouter()
+	r.HandleFunc("/permissions/users/{userId}/environments/{environmentId}", appContext.newEnvironmentPermission).Methods("GET")
+	r.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusCreated, rr.Code, "Response should be Status Created.")
+
+	mockUserDAO.AssertNumberOfCalls(t, "AssociateEnvironmentUser", 1)
+
+}
