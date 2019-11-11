b203def96c7d66b4398260982df7af9b29956909
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-11-11 11:34:19 -0300

    add permissions unit tests

--- pkg/handlers/permissions.go
+++ pkg/handlers/permissions.go
@@ -21,7 +21,7 @@ func (appContext *AppContext) newEnvironmentPermission(w http.ResponseWriter, r 
 	w.Header().Set(global.ContentType, global.JSONContentType)
 
 	vars := mux.Vars(r)
-	userID, err := strconv.Atoi(vars["userID"])
+	userID, err := strconv.Atoi(vars["userId"])
 
 	if err != nil {
 		http.Error(w, err.Error(), 501)
