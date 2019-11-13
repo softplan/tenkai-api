4f9fbe309b7fca78850c1bc1457e6fd3d29e7389
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-11-13 14:05:23 -0300

    add unit tests

--- pkg/audit/audit_test.go
+++ pkg/audit/audit_test.go
@@ -22,3 +22,16 @@ func TestGetAuditBuilder(t *testing.T) {
 	a := AuditingBuilder()
 	assert.NotNil(t, a)
 }
+
+func TestBuildDock(t *testing.T) {
+	myMap := make(map[string]string)
+	myMap["a"] = "a_value"
+	myMap["b"] = "b_value"
+	doc := buildDoc("alfa", "beta", myMap)
+	assert.NotNil(t, doc)
+}
+
+func TestBuildConfig(t *testing.T) {
+	config := buildConfig("http://audit", "alfa", "beta")
+	assert.NotNil(t, config)
+}
