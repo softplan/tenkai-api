ea0996eaac4f4bc4e745c85db044632e07d7e037
Anderson Fernando Custódio <afcustodioo@gmail.com>
2019-11-14 15:35:25 -0300

    Add tests scenarios for user repo file.

--- pkg/dbms/repository/user_test.go
+++ pkg/dbms/repository/user_test.go
@@ -116,3 +116,40 @@ func TestAssociateEnvironmentUser(t *testing.T) {
 
 	mock.ExpectationsWereMet()
 }
+
+func TestListAllUsers(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	mock.MatchExpectationsInOrder(false)
+	assert.Nil(t, err)
+
+	gormDB, err := gorm.Open("postgres", db)
+	defer gormDB.Close()
+
+	userDAO := UserDAOImpl{}
+	userDAO.Db = gormDB
+
+	payload := getUser()
+	payload.ID = 888
+
+	env := getEnvironmentTestData()
+	env.ID = 999
+
+	payload.Environments = append(payload.Environments, env)
+
+	row1 := sqlmock.NewRows([]string{"id", "email", "default_environment_id"}).AddRow(payload.ID, payload.Email, payload.DefaultEnvironmentID)
+	mock.ExpectQuery(`SELECT (.*) FROM "users"
+		WHERE "users"."deleted_at" IS NULL
+	`).WillReturnRows(row1)
+
+	row2 := sqlmock.NewRows([]string{"id"}).AddRow(env.ID)
+	mock.ExpectQuery(`SELECT (.*) FROM "environments" 
+		INNER JOIN "user_environment" ON "user_environment"."environment_id" = "environments"."id" 
+		WHERE "environments"."deleted_at" IS NULL AND \(\("user_environment"."user_id" IN (.*)\)\)
+	`).WithArgs(888).WillReturnRows(row2)
+
+	u, e := userDAO.ListAllUsers()
+	assert.NoError(t, e)
+	assert.NotNil(t, u)
+
+	mock.ExpectationsWereMet()
+}
