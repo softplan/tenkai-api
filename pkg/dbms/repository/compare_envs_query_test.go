48c9d744e611360f7f400943d0d527836eac5b84
Anderson <afcustodioo@gmail.com>
2020-02-06 10:04:27 -0300

    Add test scenarios for the compare envs query repository.

--- pkg/dbms/repository/compare_envs_query_test.go
+++ pkg/dbms/repository/compare_envs_query_test.go
@@ -1,9 +1,9 @@
 package repository
 
 import (
-	"testing"
-
 	"encoding/json"
+	"errors"
+	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/jinzhu/gorm"
@@ -12,21 +12,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateCompareEnvsQuery(t *testing.T) {
-
+func compareBeforeTest(t *testing.T) (*gorm.DB, sqlmock.Sqlmock, CompareEnvsQueryDAOImpl, model.CompareEnvsQuery) {
 	db, mock, err := sqlmock.New()
-
 	gormDB, err := gorm.Open("postgres", db)
-	defer gormDB.Close()
 
 	assert.Nil(t, err)
 
-	envDAO := CompareEnvsQueryDAOImpl{}
-	envDAO.Db = gormDB
+	dao := CompareEnvsQueryDAOImpl{}
+	dao.Db = gormDB
 
 	mock.MatchExpectationsInOrder(false)
-
-	rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
 
 	query := json.RawMessage(`{"foo":"bar"}`)
 
@@ -35,13 +30,106 @@ func TestCreateCompareEnvsQuery(t *testing.T) {
 	item.UserID = 9999
 	item.Query = postgres.Jsonb{RawMessage: query}
 
+	return gormDB, mock, dao, item
+}
+
+func TestCreateCompareEnvsQuery(t *testing.T) {
+	gormDB, mock, dao, item := compareBeforeTest(t)
+	defer gormDB.Close()
+
+	rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
+
 	mock.ExpectQuery(`INSERT INTO "compare_envs_queries"`).
-		WithArgs(item.CreatedAt, item.UpdatedAt, item.DeletedAt, item.Name, item.UserID, item.Query.RawMessage).
+		WithArgs(AnyTime{}, AnyTime{}, nil, item.Name, item.UserID, item.Query.RawMessage).
 		WillReturnRows(rows)
 
-	result, e := envDAO.SaveCompareEnvsQuery(item)
+	result, e := dao.SaveCompareEnvsQuery(item)
 	assert.Nil(t, e)
 	assert.Equal(t, 1, result)
+
+	mock.ExpectationsWereMet()
+}
+
+func TestCreateCompareEnvsQuery_Error(t *testing.T) {
+	gormDB, mock, dao, item := compareBeforeTest(t)
+	defer gormDB.Close()
+
+	mock.ExpectQuery(`INSERT INTO "compare_envs_queries"`).
+		WithArgs(AnyTime{}, AnyTime{}, nil, item.Name, item.UserID, item.Query.RawMessage).
+		WillReturnError(errors.New("some error"))
+
+	_, e := dao.SaveCompareEnvsQuery(item)
+	assert.Error(t, e)
+
+	mock.ExpectationsWereMet()
+}
+
+func TestGetByUser(t *testing.T) {
+
+	gormDB, mock, dao, item := compareBeforeTest(t)
+	defer gormDB.Close()
+
+	item.ID = 888
+	rows := sqlmock.NewRows([]string{"id", "name", "user_id", "query"}).
+		AddRow(item.ID, item.Name, item.UserID, item.Query)
+
+	item.ID = 888
+	mock.ExpectQuery(`SELECT (.+) FROM "compare_envs_queries" WHERE (.+)`).
+		WithArgs(888).WillReturnRows(rows)
+
+	list, err := dao.GetByUser(888)
+	assert.Nil(t, err)
+	assert.NotNil(t, list)
+
+	mock.ExpectationsWereMet()
+}
+
+func TestGetByUser_NotFound(t *testing.T) {
+
+	gormDB, mock, dao, item := compareBeforeTest(t)
+	defer gormDB.Close()
+
+	item.ID = 888
+
+	item.ID = 888
+	mock.ExpectQuery(`SELECT (.+) FROM "compare_envs_queries" WHERE (.+)`).
+		WithArgs(888).WillReturnError(gorm.ErrRecordNotFound)
+
+	result, err := dao.GetByUser(888)
+	assert.Nil(t, err)
+	assert.Empty(t, result)
+
+	mock.ExpectationsWereMet()
+}
+
+func TestGetByUser_Error(t *testing.T) {
+
+	gormDB, mock, dao, item := compareBeforeTest(t)
+	defer gormDB.Close()
+
+	item.ID = 888
+
+	item.ID = 888
+	mock.ExpectQuery(`SELECT (.+) FROM "compare_envs_queries" WHERE (.+)`).
+		WithArgs(888).WillReturnError(errors.New("mock error"))
+
+	_, err := dao.GetByUser(888)
+	assert.Error(t, err)
+
+	mock.ExpectationsWereMet()
+}
+
+func TestDeleteCompareEnvQuery(t *testing.T) {
+
+	gormDB, mock, dao, item := compareBeforeTest(t)
+	defer gormDB.Close()
+
+	item.ID = 999
+	mock.ExpectExec(`DELETE FROM "compare_envs_queries" WHERE (.*)`).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err := dao.DeleteCompareEnvQuery(999)
+	assert.Nil(t, err)
 
 	mock.ExpectationsWereMet()
 }
