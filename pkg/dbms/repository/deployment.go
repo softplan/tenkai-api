0509995bf182f9c715e630fcca1768e25e3f2378
Wesley de Freitas <wesley.paulino@softplan.com.br>
2020-11-03 09:45:42 -0300

    Add deployment table, add deployment's id on rabbit payload, update tests

---
+++ pkg/dbms/repository/deployment.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/softplan/tenkai-api/pkg/dbms/model"
+)
+
+//DeploymentDAOInterface DeploymentDAOInterface
+type DeploymentDAOInterface interface {
+	CreateDeployment(deployment model.Deployment) (int, error)
+	EditDeployment(deployment model.Deployment) (error)
+	ListDeploymentByUserID(userID int) ([]model.Deployment, error)
+}
+
+//DeploymentDAOImpl DeploymentDAOImpl
+type DeploymentDAOImpl struct {
+	Db *gorm.DB
+}
+
+//CreateDeployment create deployment
+func (dao DeploymentDAOImpl) CreateDeployment(deployment model.Deployment) (int, error) {
+	if err := dao.Db.Create(&deployment).Error; err != nil {
+		return -1, err
+	}
+	return int(deployment.ID), nil
+}
+
+//EditDeployment edit deployment
+func (dao DeploymentDAOImpl) EditDeployment(deployment model.Deployment) (error) {
+	return dao.Db.Save(&deployment).Error
+}
+
+//ListDeploymentByUserID list all deployments
+func (dao DeploymentDAOImpl) ListDeploymentByUserID(userID int) ([]model.Deployment, error) {
+	deployments := make([]model.Deployment, 0)
+	err := dao.Db.Find(&deployments).Error
+	if err != nil {
+		return deployments, nil
+	}
+	return deployments, err
+}
---
+++ pkg/dbms/repository/deployment_test.go
@@ -0,0 +1,63 @@
+package repository
+
+import (
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jinzhu/gorm"
+	model2 "github.com/softplan/tenkai-api/pkg/dbms/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func getDeployment() (model2.Deployment){
+	now := time.Now()
+	deployment := model2.Deployment{}
+	deployment.CreatedAt = now
+	deployment.UpdatedAt = now
+	deployment.DeletedAt = nil
+	deployment.Chart = "Chart Teste"
+	deployment.Status = true
+	deployment.Message = "Message teste"
+	deployment.Environment = 1
+	deployment.User = 1
+	
+	return deployment
+}
+
+func TestCreateDeployment(test *testing.T) {
+	db, mock, err := sqlmock.New()
+	mock.MatchExpectationsInOrder(false)
+	assert.Nil(test, err)
+
+	gormDB, err := gorm.Open("postgres", db)
+	defer gormDB.Close()
+
+	deploymentDAO := DeploymentDAOImpl{}
+	deploymentDAO.Db = gormDB
+
+	deployment := getDeployment()
+
+	rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
+
+	mock.ExpectQuery(
+		`INSERT INTO "deployments"`,
+		).WithArgs(
+			AnyTime{}, 
+			AnyTime{}, 
+			nil,
+			deployment.Environment,
+			deployment.Chart,
+			deployment.User,
+			deployment.Status,
+			deployment.Message,
+		).WillReturnRows(rows)
+	
+		_, err = deploymentDAO.CreateDeployment(deployment)
+		
+
+	assert.Nil(test, err)
+	mock.ExpectationsWereMet()
+}
+
+
