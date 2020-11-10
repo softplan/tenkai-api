fe05b948e087aea415c1f65197c2640520914da8
Wesley de Freitas <wesley.paulino@softplan.com.br>
2020-11-10 17:50:39 -0300

    Update struct field names and json tag

--- pkg/dbms/repository/deployment.go
+++ pkg/dbms/repository/deployment.go
@@ -79,13 +79,13 @@ func (dao DeploymentDAOImpl) ListDeployments(startDate, endDate, userID, environ
 		deployment.ID = uint(id)
 		deployment.CreatedAt = createdAt
 		deployment.UpdatedAt = updatedAt
-		deployment.ChartResponse = chart
+		deployment.ChartName = chart
 		deployment.Environment.ID = uint(envID)
 		deployment.Environment.Name = envName
 		deployment.User.ID = uint(userID)
 		deployment.User.Email = userEmail
-		deployment.SuccessResponse = success
-		deployment.MessageResponse = message
+		deployment.SuccessDeployment = success
+		deployment.ErrorMessage = message
 
 		deployments = append(deployments, deployment)
 	}
