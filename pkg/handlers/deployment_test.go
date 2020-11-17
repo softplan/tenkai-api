782177a7d549a308322aa019faa73f9c30c620d6
Wesley de Freitas <wesley.paulino@softplan.com.br>
2020-11-17 15:03:39 -0300

    Add more tests to deployment

--- pkg/handlers/deployment_test.go
+++ pkg/handlers/deployment_test.go
@@ -44,7 +44,7 @@ func TestListDeploymentsWithRightParams(test *testing.T) {
 
 	req, err := http.NewRequest(
 		"GET",
-		"/deployments?start_date=2020-01-01&end_date=2020-01-01&user_id=1&environment_id=1&pageSize=10",
+		"/deployments?start_date=2020-01-01&end_date=2020-01-01&user_id=1&environment_id=1&pageSize=10&page=1",
 		nil,
 	)
 	if err != nil {
@@ -80,11 +80,71 @@ func TestListDeploymentsWrongPageNumber(test *testing.T) {
 	assert.Equal(test, http.StatusBadRequest, rr.Result().StatusCode)
 }
 
+func TestListDeploymentsWithPageSizeNotNumber(test *testing.T) {
+
+	req, err := http.NewRequest(
+		"GET",
+		"/deployments?start_date=2020-01-01&end_date=2020-01-01&pageSize=a",
+		nil,
+	)
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	appContext := getAppContext()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(appContext.listDeployments)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(test, http.StatusBadRequest, rr.Result().StatusCode)
+}
+
 func TestListDeploymentsWithoutEndDate(test *testing.T) {
 
 	req, err := http.NewRequest(
 		"GET",
 		"/deployments?start_date=2020-01-01",
+		nil,
+	)
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	appContext := getAppContext()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(appContext.listDeployments)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(test, http.StatusBadRequest, rr.Result().StatusCode)
+}
+
+func TestListDeploymentsWithWrongStartDateFormat(test *testing.T) {
+
+	req, err := http.NewRequest(
+		"GET",
+		"/deployments?start_date=2020-01&end_date=2020-01-01",
+		nil,
+	)
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	appContext := getAppContext()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(appContext.listDeployments)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(test, http.StatusBadRequest, rr.Result().StatusCode)
+}
+
+func TestListDeploymentsWithWrongEndDateFormat(test *testing.T) {
+
+	req, err := http.NewRequest(
+		"GET",
+		"/deployments?start_date=2020-01-01&end_date=2020-01",
 		nil,
 	)
 	if err != nil {
