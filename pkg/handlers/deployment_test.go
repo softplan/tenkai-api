76f43362341ddbd331792af2bc0ee89389655c23
Wesley de Freitas <wesley.paulino@softplan.com.br>
2020-11-17 13:12:09 -0300

    Add more tests to deployment's handler

--- pkg/handlers/deployment_test.go
+++ pkg/handlers/deployment_test.go
@@ -44,7 +44,7 @@ func TestListDeploymentsWithRightParams(test *testing.T) {
 
 	req, err := http.NewRequest(
 		"GET",
-		"/deployments?start_date=2020-01-01&end_date=2020-01-01&user_id=1&environment_id=1",
+		"/deployments?start_date=2020-01-01&end_date=2020-01-01&user_id=1&environment_id=1&pageSize=10",
 		nil,
 	)
 	if err != nil {
@@ -58,6 +58,66 @@ func TestListDeploymentsWithRightParams(test *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	assert.Equal(test, http.StatusOK, rr.Result().StatusCode)
+}
+
+func TestListDeploymentsWrongPageNumber(test *testing.T) {
+
+	req, err := http.NewRequest(
+		"GET",
+		"/deployments?start_date=2020-01-01&end_date=2020-01-01&user_id=1&environment_id=1&page=a",
+		nil,
+	)
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	appContext := getAppContext()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(appContext.listDeployments)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(test, http.StatusBadRequest, rr.Result().StatusCode)
+}
+
+func TestListDeploymentsWithoutEndDate(test *testing.T) {
+
+	req, err := http.NewRequest(
+		"GET",
+		"/deployments?start_date=2020-01-01",
+		nil,
+	)
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	appContext := getAppContext()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(appContext.listDeployments)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(test, http.StatusBadRequest, rr.Result().StatusCode)
+}
+
+func TestListDeploymentsWithoutStartDate(test *testing.T) {
+
+	req, err := http.NewRequest(
+		"GET",
+		"/deployments?end_date=2020-01-01",
+		nil,
+	)
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	appContext := getAppContext()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(appContext.listDeployments)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(test, http.StatusBadRequest, rr.Result().StatusCode)
 }
 
 func TestListDeploymentsWithoutParams(test *testing.T) {
